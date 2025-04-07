356fea98af3d6d97dc289ef4f93c9e8bd9a0c1ee
AakashHarsh <79820448+AakashHarsh@users.noreply.github.com>
2025-04-07 23:23:14 +0530

    Add files via upload

---
+++ bvm_project/bvm/exceptions.py
@@ -0,0 +1,14 @@
+class VMException(Exception):
+    pass
+
+class OutOfGasError(VMException):
+    pass
+
+class StackUnderflowError(VMException):
+    pass
+
+class StackOverflowError(VMException):
+    pass
+
+class InvalidOpcodeError(VMException):
+    pass
